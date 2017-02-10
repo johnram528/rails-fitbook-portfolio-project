c7d780c19a47aa366953ce9853abaa538b33d2d9
johnram528 <johnram528@gmail.com>
2017-02-10 15:31:19 -0500

    fixed redirect typo in User#edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,18 @@ class UsersController < ApplicationController
     if current_user
       @user = User.find_by_id(current_user.id)
     else
-      redirect_to new_session_path
+      redirect_to new_user_session_path
     end
   end
 
   def update
+    @user = User.find_by_id(current_user.id)
+    @user.update(user_params)
+    redirect_to root_path
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:height, :weight)
+    end
 end
