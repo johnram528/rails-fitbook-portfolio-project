7fb6557599d0cb439dbd8edfa240b7a6d5d81b68
johnram528 <johnram528@gmail.com>
2017-02-09 21:35:20 -0500

    associations added to models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,4 @@
 class Exercise < ActiveRecord::Base
+  has_many :routine_exercises
+  has_many :routines, through: :workout_exercises
 end
--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,2 +1,5 @@
 class Routine < ActiveRecord::Base
+  has_many :routine_exercises
+  has_many :routines, through: :workout_exercises
+  belongs_to :user
 end
