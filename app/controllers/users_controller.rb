961b88bb87b0a42e0687512adc043ae10252e3ae
johnram528 <johnram528@gmail.com>
2017-02-10 02:16:17 -0500

    created user#edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
+ 
   def show
   end
 
   def edit
+    if current_user
+      @user = User.find_by_id(current_user.id)
+    else
+      redirect_to new_session_path
+    end
   end
 
   def update
