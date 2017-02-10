6bf91cb2819bcf5605ae5aeff105bdf23fbcbe2e
johnram528 <johnram528@gmail.com>
2017-02-09 22:45:19 -0500

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :routines
+  resources :exercises
+  resources :users, only: [:new, :create, :edit, :update, :show]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
