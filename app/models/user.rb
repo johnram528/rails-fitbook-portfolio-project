3ab0004a633983d6519dc0f36b3de8169b9186fd
johnram528 <johnram528@gmail.com>
2017-02-11 15:06:46 -0500

    added health_status method to user model and filled out user show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,15 @@ class User < ActiveRecord::Base
     bmi = metric_weight/sqrt_metric_height
     bmi.round
   end
+
+  def health_status
+    if bmi > 18 && bmi < 25
+      "You're at at a healthy weight. Keep up the good work!"
+    else 
+      "Every workout counts. Keep up the good work!"
+    end
+  end
+
 end
 
 
