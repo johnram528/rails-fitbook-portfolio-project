646c541798bd044a6bb4e37f883a0178951bc2e5
johnram528 <johnram528@gmail.com>
2017-02-10 19:47:34 -0500

    added time methods to routine model and view

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,4 +2,16 @@ class Routine < ActiveRecord::Base
   has_many :routine_exercises
   has_many :exercises, through: :routine_exercises
   belongs_to :user
+
+  def estimated_time
+    exercises.map(&:estimated_time).inject(45, :+) unless exercises.empty? #accounts for 45 second break between exercise.
+  end
+
+  def show_estimated_time
+    if estimated_time > 60
+      "#{estimated_time/60} min"
+    else
+      "#{estimated_time} sec"
+    end
+  end
 end
