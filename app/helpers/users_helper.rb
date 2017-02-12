a7a341d3431ae9c1ea98883f29806fe7079ad4da
johnram528 <johnram528@gmail.com>
2017-02-12 17:27:25 -0500

    revised user helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def health_status
-    if bmi > 18 && bmi < 25
+  def health_status(user)
+    if user.bmi > 18 && user.bmi < 25
       "You're at at a healthy weight. Keep up the good work!"
     else 
       "Every workout counts. Keep up the good work!"
