352856fcff6df37e8dae70431a72ff72d6d90045
johnram528 <johnram528@gmail.com>
2017-02-13 02:13:21 -0500

    using partial for exercise portion of routine show view

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -3,7 +3,6 @@ class Routine < ActiveRecord::Base
   has_many :exercises, through: :routine_exercises
   belongs_to :user, inverse_of: :routines
 
-  # accepts_nested_attributes_for :exercises, :allow_destroy => true, :reject_if => :all_blank
   validates_presence_of :exercises
 
   def estimated_time
